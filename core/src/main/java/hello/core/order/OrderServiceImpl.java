fad6b28432988d68f05a9be32faf61c1a5fd371a
jeawook <pjw4825@naver.com>
2020-11-25 20:37:07 +0900

    init commit

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+
+	private final MemberRepository memberRepository;
+	private final DiscountPolicy discountPolicy;
+
+
+	@Override
+	public Order createOrder(Long memberId, String itemName, int itemPrice) {
+		// TODO Auto-generated method stub
+		Member member= memberRepository.findById(memberId);
+		int discountPrice = discountPolicy.discount(member, itemPrice);
+		return new Order(memberId, itemName, itemPrice, discountPrice);
+	}
+
+}
