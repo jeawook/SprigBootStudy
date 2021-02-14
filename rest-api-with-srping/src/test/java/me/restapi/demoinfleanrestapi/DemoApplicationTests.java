daed9590a1d9006f22a0d0bc21751edb818e89c6
jeawook <pjw4825@naver.com>
2021-02-14 16:10:44 +0900

    project creation

---
+++ rest-api-with-srping/src/test/java/me/restapi/demoinfleanrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.restapi.demoinfleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
