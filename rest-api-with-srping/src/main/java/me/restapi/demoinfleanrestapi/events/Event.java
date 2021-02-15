f55689480908ab27e358de79585f78130fc4c7f8
jeawook <pjw4825@naver.com>
2021-02-15 15:12:21 +0900

    POST /api/events returns 201

--- rest-api-with-srping/src/main/java/me/restapi/demoinfleanrestapi/events/Event.java
+++ rest-api-with-srping/src/main/java/me/restapi/demoinfleanrestapi/events/Event.java
@@ -2,12 +2,15 @@ package me.restapi.demoinfleanrestapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -20,6 +23,7 @@ public class Event {
     private int maxPrice;
     private int limitOfEnrollment;
     private boolean offline;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
--- rest-api-with-srping/src/test/java/me/restapi/demoinfleanrestapi/events/EventControllerTests.java
+++ rest-api-with-srping/src/test/java/me/restapi/demoinfleanrestapi/events/EventControllerTests.java
@@ -4,8 +4,10 @@ package me.restapi.demoinfleanrestapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +29,8 @@ public class EventControllerTests {
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
+    @MockBean
+    EventRepository eventRepository;
     @Autowired
     private WebApplicationContext ctx;
 
@@ -54,7 +58,8 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타트업 팩토리")
                 .build();
-
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaTypes.HAL_JSON)
