bd28365a59c434df8dd512304f71ca4abe0d9964
jeawook <pjw4825@naver.com>
2021-02-15 21:55:55 +0900

    validate wrong data

---
+++ rest-api-with-srping/src/main/java/me/restapi/demoinfleanrestapi/events/EventValidator.java
@@ -0,0 +1,27 @@
+package me.restapi.demoinfleanrestapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDTO eventDTO, Errors errors) {
+        if (eventDTO.getBasePrice() > eventDTO.getMaxPrice() && eventDTO.getMaxPrice() > 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+            LocalDateTime endEventDateTime = eventDTO.getEndEventDateTime();
+            if (endEventDateTime.isBefore(eventDTO.getBeginEventDateTime()) ||
+            endEventDateTime.isBefore(eventDTO.getCloseEnrollmentDateTime()) ||
+            endEventDateTime.isBefore(eventDTO.getBeginEnrollmentDateTime()) ) {
+                errors.rejectValue("endEventDateTime", "wrongValue", "EndEventDateTime is wrong");
+            }
+
+            // TODO BeginEventDateTime
+            // TODO CloseEnrollmentDateTime
+
+        }
+    }
+}
