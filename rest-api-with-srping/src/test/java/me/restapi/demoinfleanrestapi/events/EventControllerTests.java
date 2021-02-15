201cb147f71c777d7fc70451abd405ce9c265d50
jeawook <pjw4825@naver.com>
2021-02-15 22:02:39 +0900

    Add test description annotation

--- rest-api-with-srping/src/test/java/me/restapi/demoinfleanrestapi/events/EventControllerTests.java
+++ rest-api-with-srping/src/test/java/me/restapi/demoinfleanrestapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package me.restapi.demoinfleanrestapi.events;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.restapi.demoinfleanrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,6 +48,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDTO event = EventDTO.builder()
                 .name("Spring")
@@ -74,6 +76,7 @@ public class EventControllerTests {
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
     }
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생 하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -100,6 +103,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDTO eventDTO = EventDTO.builder().build();
 
@@ -108,7 +112,9 @@ public class EventControllerTests {
                     .content(this.objectMapper.writeValueAsString(eventDTO)))
                 .andExpect(status().isBadRequest());
     }
+
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDTO eventDTO = EventDTO.builder()
                 .name("Spring")
