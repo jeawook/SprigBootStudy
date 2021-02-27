6faf724b9ce04093a330d174be0910f82ed4babe
jeawook <pjw4825@naver.com>
2021-02-27 15:15:04 +0900

    add auditing

--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,20 +1,26 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 class MemberTest {
 
     @PersistenceContext
     EntityManager em;
-
+    @Autowired
+    MemberRepository memberRepository;
     @Test
     public void testEntity() {
         Team teamA = new Team("teamA");
@@ -44,6 +50,21 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team = " + member.getTeam());
         }
+
+
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception{
+        Member member = new Member("member1");
+        memberRepository.save(member);
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+        Optional<Member> findMember = memberRepository.findById(member.getId());
+
 
 
     }
