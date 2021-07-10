2935e222e6c3945277bb9eb3de8076fc5bc4f041
jeawook <pjw4825@naver.com>
2021-07-10 17:51:52 +0900

    commit servlet 1

--- servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
@@ -23,7 +26,20 @@ class MemberRepositoryTest {
 
         Member findMember = memberRepository.find(save.getId());
 
-        Assertions.assertThat(findMember).isEqualTo(save);
+        assertThat(findMember).isEqualTo(save);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
     }
 
 }
