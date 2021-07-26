d04e3aa58a35745dba8f937d1b5ae2391b65a634
jeawook <pjw4825@naver.com>
2021-07-26 17:29:54 +0900

    spring mvc basic commit

--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class LogTestController {
-    
+
     @RequestMapping("/log-test")
     public String logTest() {
         String name = "Spring";
